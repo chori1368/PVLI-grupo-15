33966568d979818b9ea4c4f05471362024a47e8a
tomastur3 <199897750+TomasArevalo3@users.noreply.github.com>
2025-12-05 16:35:08 +0100

    wfwfw

--- src/player/player_spear.js
+++ src/player/player_spear.js
@@ -7,8 +7,6 @@ export default class PlayerSpear extends Player {
         this.jumpSpeed = -750;
         this.dashing = false;
         this.type = 1; //spear type
-        this.horizontalbox = scene.add.zone(0, 0, 110, 30);
-        this.verticalbox = scene.add.zone(0, 0, 55, 80);
 
     }
 
--- src/player/player_sword.js
+++ src/player/player_sword.js
@@ -4,7 +4,6 @@ export default class PlayerSword extends Player {
     constructor(scene, side) { 
         super(scene, side, 'sword');
         this.type = 0; //sword type
-        this.verticalbox = scene.add.zone(0, 0, 55, 100);
 
     }
 
